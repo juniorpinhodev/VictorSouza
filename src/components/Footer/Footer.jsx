9fed39859fdddac52133657e990d824687dc9b94
juniorpinhodev <juniorpinhodev@gmail.com>
2025-04-28 15:46:44 -0300

    feat: 🎸 Organiza footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -37,25 +37,15 @@ const Footer = () => {
                 initial={{ opacity: 0, y: 100 }}
                 whileInView={{ opacity: 1, y:0 }}
                 transition={{ delay: 0.6, duration: 0.5 }}
-                className="space-y-6"> 
+                className="space-y-6 text-center"> 
                 <h1 className="text-3xl font-bold"> Menu</h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
-                    <div>
-                        <ul>
-                            <li>Home</li>
-                            <li>Serviços</li>
-                            <li>Projects</li>
-                            <li>About</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <ul>
-                            <li>Home</li>
-                            <li>Services</li>
-                            <li>Projects</li>
-                            <li>About</li>
-                        </ul>
-                    </div>
+                <div className="flex justify-center">
+                    <ul className="flex flex-col items-center gap-1">
+                        <li>Home</li>
+                        <li>Sobre</li>
+                        <li>Serviços</li>
+                        <li>Blogs</li>
+                    </ul>
                 </div>
             </motion.div>
             {/* <!-- redes sociais --> */}
